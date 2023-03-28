8c4ab46b3d33c5357c6c0acde8b889563e6ad06f
emilie <emilieroger360@gmail.com>
2023-03-28 15:22:06 +0200

    v1

---
+++ front/assets/js/scriptfront.js
@@ -0,0 +1,15 @@
+// Partie Page d'accueil (index)
+
+// Partie Page de connexion/inscription (login)
+
+// Partie Page Profile (profil)
+
+// Partie Page de recherche (search)
+
+// Partie Page paramètres (parameters)
+
+// Partie Page contact et mentions légales (contact)
+
+// Partie Page messagerie (messages)
+
+// Partie Page blogs (blogs)
---
+++ front/pages/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/stylefront.css" />
+    <title>Connexion/Inscription</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <div class="container">
+        <form action="" method="post" name="form-login">
+            <label for="loginname">Nom :</label>
+            <input type="text" name="loginname" id="loginname">
+            <label for="login">Prénom :</label>
+            <input type="text" name="login" id="login">
+            <label for="loginemail">Email :</label>
+            <input type="text" name="loginemail" id="loginemail">
+            <label for="loginpassword">Mot de passe :</label>
+            <input type="text" name="loginpassword" id="loginpassword">
+            <label for="loginpasswordr">Répéter votre mot de passe</label>
+            <input type="text" name="loginpasswordr" id="loginpasswordr">
+            <button type="reset">Annuler</button>
+            <button type="submit">Valider</button>
+        </form>
+        <form action="" method="post" name="form-register">
+            <label for="registeremail">Email :</label>
+            <input type="text" name="registeremail" id="registeremail">
+            <label for="registerpassword">Mot de passe :</label>
+            <input type="text" name="registerpassword" id="registerpassword">
+            <button type="reset">Annuler</button>
+            <button type="submit">Valider</button>
+        </form>
+    </div>
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="../assets/js/scriptfront.js"></script>
+</body>
+
+</html>
